eaa9ebca87df05f653e20206223323938fb7d6a7
7char7 <7charlotte72@gmail.com>
2025-11-19 19:15:51 -0500

    C++ scripts

---
+++ SheepyStudios/Intermediate/Build/Win64/UnrealEditor/Inc/SheepyStudios/UHT/Test_Script2.generated.h
@@ -0,0 +1,60 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+// IWYU pragma: private, include "Test_Script2.h"
+
+#ifdef SHEEPYSTUDIOS_Test_Script2_generated_h
+#error "Test_Script2.generated.h already included, missing '#pragma once' in Test_Script2.h"
+#endif
+#define SHEEPYSTUDIOS_Test_Script2_generated_h
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+
+// ********** Begin Class ATest_Script2 ************************************************************
+SHEEPYSTUDIOS_API UClass* Z_Construct_UClass_ATest_Script2_NoRegister();
+
+#define FID_Users_rappa_Documents_Github_Sheepy_Studios_SheepyStudios_Source_SheepyStudios_Test_Script2_h_12_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesATest_Script2(); \
+	friend struct Z_Construct_UClass_ATest_Script2_Statics; \
+	static UClass* GetPrivateStaticClass(); \
+	friend SHEEPYSTUDIOS_API UClass* Z_Construct_UClass_ATest_Script2_NoRegister(); \
+public: \
+	DECLARE_CLASS2(ATest_Script2, AActor, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/SheepyStudios"), Z_Construct_UClass_ATest_Script2_NoRegister) \
+	DECLARE_SERIALIZER(ATest_Script2)
+
+
+#define FID_Users_rappa_Documents_Github_Sheepy_Studios_SheepyStudios_Source_SheepyStudios_Test_Script2_h_12_ENHANCED_CONSTRUCTORS \
+	/** Deleted move- and copy-constructors, should never be used */ \
+	ATest_Script2(ATest_Script2&&) = delete; \
+	ATest_Script2(const ATest_Script2&) = delete; \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ATest_Script2); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ATest_Script2); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(ATest_Script2) \
+	NO_API virtual ~ATest_Script2();
+
+
+#define FID_Users_rappa_Documents_Github_Sheepy_Studios_SheepyStudios_Source_SheepyStudios_Test_Script2_h_9_PROLOG
+#define FID_Users_rappa_Documents_Github_Sheepy_Studios_SheepyStudios_Source_SheepyStudios_Test_Script2_h_12_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_Users_rappa_Documents_Github_Sheepy_Studios_SheepyStudios_Source_SheepyStudios_Test_Script2_h_12_INCLASS_NO_PURE_DECLS \
+	FID_Users_rappa_Documents_Github_Sheepy_Studios_SheepyStudios_Source_SheepyStudios_Test_Script2_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+class ATest_Script2;
+
+// ********** End Class ATest_Script2 **************************************************************
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_Users_rappa_Documents_Github_Sheepy_Studios_SheepyStudios_Source_SheepyStudios_Test_Script2_h
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
